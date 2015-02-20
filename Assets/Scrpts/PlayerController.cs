f2b18e543faf84dc8e4caeef099e72b83383abec
海下 直哉 <naoya-kaige@garage.co.jp>
2015-02-20 17:22:20 +0900

    first commit

---
+++ Assets/Scrpts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed;
+	public Text countText, winText;
+	private int count;
+	private const int pickUpLength = 8;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count == pickUpLength) {
+			winText.text = "You Win!!";
+		} 
+		else 
+		{
+			winText.text = "";
+		}
+	}
+}
